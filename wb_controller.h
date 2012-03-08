72c1022da0bc1c1dda222904c32bdd4fe8ef52c2
nifigager@gmail.com <nifigager@gmail.com@2692835d-f07e-b6f2-626d-22f6840c62f8>
2012-03-08 13:40:29 +0000

    Added license, ccplit, word dictionary, empty header and source files

---
+++ wb_controller.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2012 Ilya Tetin and Danila Shikulin. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WB_CONTROLLER_H_
+#define WB_CONTROLLER_H_
+
+
+
+#endif  // WB_CONTROLLER_H_
